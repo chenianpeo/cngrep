e44f99b68d08a66f6c1edefdf93954051305a9e2
chenian <chenianpeo@163.com>
2026-06-11 21:48:27 +0800

    fix(reader): use custom replaced unwrap

--- src/reader.rs
+++ src/reader.rs
@@ -1,16 +1,19 @@
 use std::fs::{self, File};
-use std::io::BufReader;
+use std::io::{BufReader, Error};
 
 use crate::config::{CliArgs, ReadResult};
 use crate::error::CnError;
 
 impl CliArgs {
     pub fn read(&self) -> Result<ReadResult, CnError> {
-        let open_file = File::open(&self.path).unwrap();
+        let open_file = File::open(&self.path)?;
         let file_content = BufReader::new(open_file);
 
-        let absolute_path = fs::canonicalize(&self.path).unwrap();
-        let file_path = absolute_path.to_str().unwrap().to_string();
+        let absolute_path = fs::canonicalize(&self.path)?;
+        let file_path = absolute_path
+            .to_str()
+            .ok_or(CnError::Io(Error::new(std::io::ErrorKind::Other, "None")))?
+            .to_string();
 
         Ok(ReadResult {
             path: file_path,
