2fc0420dfd5ca529ac811e2943b08a6fafc2ef3b
chenian <chenianpeo@163.com>
2026-06-12 23:36:00 +0800

    fix(error): remove unused error: CnError

--- src/error.rs
+++ src/error.rs
@@ -1,32 +1,6 @@
 use std::fmt;
 use std::io;
 
-// define custom error enum
-#[derive(Debug)]
-pub enum CnError {
-    Io(io::Error),
-    Parse(String),
-    Custom(String),
-}
-
-impl fmt::Display for CnError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            CnError::Io(e) => write!(f, "io error: {}", e),
-            CnError::Parse(msg) => write!(f, "parse error: {}", msg),
-            CnError::Custom(msg) => write!(f, "custom return: {}", msg),
-        }
-    }
-}
-
-impl std::error::Error for CnError {}
-
-impl From<io::Error> for CnError {
-    fn from(err: io::Error) -> Self {
-        CnError::Io(err)
-    }
-}
-
 #[derive(Debug)]
 pub enum Error {
     InvalidPattern {
@@ -50,7 +24,10 @@ impl std::error::Error for Error {}
 
 impl From<io::Error> for Error {
     fn from(err: io::Error) -> Self {
-        Error::Io { source: err, context: None }
+        Error::Io {
+            source: err,
+            context: None,
+        }
     }
 }
 
