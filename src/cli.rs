1fd172a14601b1acce9130dfdde1289b7d9bcfb2
chenian <chenianpeo@163.com>
2026-06-14 00:56:14 +0800

    feat(cli): judge if exist mode

--- src/cli.rs
+++ src/cli.rs
@@ -1,19 +1,19 @@
-use crate::{config::CliArgs, error::Error};
+use crate::{config::{self, CliArgs}, error::Error};
 
 pub fn args() -> Result<CliArgs, Error> {
     let args: Vec<String> = std::env::args().collect();
 
-    if args.len() != 3 {
-        return Err(Error::InvalidArgument {
-            r#type: "length".to_string(),
-            reason: "args length isn't 2".to_string(),
-        });
+    let mode = config::Mode::Normal;
+    match args.len() {
+        3 => return Ok(CliArgs { query: args[1].clone(), file: args[2].clone(), mode }),
+        4 => {
+            if args[1].clone() == "-c".to_string() {
+                let mode = config::Mode::CountOnly;
+                return Ok(CliArgs { query: args[2].clone(), file: args[3].clone(), mode })
+            } else {
+                return Err(Error::InvalidArgument { r#type: "parse".to_string(), reason: "unknown command".to_string() })
+            }
+        }
+        _ => return Err(Error::InvalidArgument { r#type: "length".to_string(), reason: "args length isn't 2".to_string() }),
     }
-
-    let cli_args = CliArgs {
-        query: args[1].clone(),
-        file: args[2].clone(),
-    };
-
-    Ok(cli_args)
 }
