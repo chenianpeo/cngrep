2c44b88c1c2feb1e8bed0cb7feca608410291df1
chenian <chenianpeo@163.com>
2026-06-15 21:03:36 +0800

    feat(config): design arguments

--- src/config.rs
+++ src/config.rs
@@ -1,11 +1,33 @@
 use std::fs::File;
 use std::io::BufReader;
+use std::path::PathBuf;
 
 #[derive(Debug)]
 pub struct CliArgs {
     pub query: String,
     pub file: String,
     pub mode: Mode,
+}
+
+#[derive(Debug)]
+pub struct Cli {
+    pub query: String,
+    pub file: Option<PathBuf>,
+
+    pub count: bool,
+}
+
+pub struct Args {
+    pub query: String,
+    pub input_source: InputSource,
+    pub mode: Mode,
+}
+
+#[derive(Debug)]
+pub enum InputSource {
+    File(PathBuf),
+    Stdin,
+    CurrentDir,
 }
 
 #[derive(Debug)]
@@ -19,6 +41,7 @@ pub struct ReadResult {
     pub path: String,
     pub file: BufReader<File>,
 }
+
 #[derive(Debug)]
 pub struct MatchResult {
     pub path: String,
