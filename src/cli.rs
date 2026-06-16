5228d5fe56f990561a77ec45266537a80bf055d5
chenian <chenianpeo@163.com>
2026-06-16 11:23:32 +0800

    feat(cli): accept arguments

--- src/cli.rs
+++ src/cli.rs
@@ -41,11 +41,37 @@ pub fn _args() -> Result<Cli, crate::error::Error> {
     let args: Vec<String> = std::env::args().collect();
 
     match args.len() {
+        2 => Ok(Cli {
+            query: args[1].clone(),
+            file: None,
+            count: false,
+        }),
         3 => Ok(Cli {
             query: args[1].clone(),
             file: Some(PathBuf::from(&args[2])),
             count: false,
         }),
+        4 => {
+            if args[1].clone() == "-c" {
+                Ok(Cli {
+                    query: args[2].clone(),
+                    file: Some(PathBuf::from(&args[3])),
+                    count: true,
+                })
+            } else if args[3].clone() == "-c" {
+                Ok(Cli {
+                    query: args[1].clone(),
+                    file: Some(PathBuf::from(&args[2])),
+                    count: true,
+                })
+            } else {
+                Err(Error::InvalidArgument {
+                    r#type: "option".to_string(),
+                    reason: "unknown arguments".to_string(),
+                })
+            }
+        }
+
         _ => Err(Error::InvalidArgument {
             r#type: "arguments".to_string(),
             reason: "failed command".to_string(),
