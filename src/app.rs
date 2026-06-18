b8fadbffeb5d35a0ab52a7acf53a8b82613def67
chenian <chenianpeo@163.com>
2026-06-18 22:56:20 +0800

    feat(app): redesign running flow

--- src/app.rs
+++ src/app.rs
@@ -1,10 +1,10 @@
 use crate::cli::args;
-use crate::config::{Args, InputSource, MatchResult};
+use crate::config::{Args, InputSource};
 use crate::error::Error;
+use crate::matcher::{FileMatch, Match};
 use crate::reader::{FileReader, Reader};
 
 pub fn run() -> Result<(), Error> {
-    use std::io::BufRead;
     let cli = args()?;
 
     let args = Args::from_cli(cli)?;
@@ -12,27 +12,24 @@ pub fn run() -> Result<(), Error> {
     match args.input_source {
         InputSource::File(path) => {
             let file = FileReader { path };
+
             let content = file.read()?;
 
-            let mut search_result: Vec<MatchResult> = Vec::new();
+            let mut file_match = FileMatch {
+                query: args.query,
+                file: content,
+            };
 
-            for (line_no, line) in content.lines().enumerate() {
-                let line = line?;
-                let line_no = line_no + 1;
-
-                if line.contains(&args.query.to_string()) {
-                    search_result.push(MatchResult {
-                        path: "path".to_string(),
-                        line_no,
-                        content: line,
-                    })
-                }
-            }
-
+            let search_result = file_match.search()?;
             println!("{:#?}", search_result);
         }
-        _ => println!("undone"),
-    }
+        InputSource::Stdin => {
+            println!("stdin");
+        }
+        InputSource::CurrentDir => {
+            println!("current dir")
+        }
+    };
 
     Ok(())
 }
