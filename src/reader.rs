c13f213d07fdd501fe49d318442ad34b5dda5bf3
chenian <chenianpeo@163.com>
2026-06-18 21:35:08 +0800

    refactor(reader): remove unused code

--- src/reader.rs
+++ src/reader.rs
@@ -1,31 +1,6 @@
-use std::fs::{self, File};
-use std::io::{self, BufReader};
+use std::fs::File;
+use std::io::BufReader;
 use std::path::PathBuf;
-
-use crate::config::{CliArgs, ReadResult};
-use crate::error::Error;
-
-impl CliArgs {
-    pub fn read(&self) -> Result<ReadResult, crate::error::Error> {
-        let open_file = File::open(&self.file).map_err(|_| Error::Io {
-            source: io::Error::new(io::ErrorKind::NotFound, (self.file).to_string()),
-            context: Some("failed to read".to_string()),
-        })?;
-
-        let file_content = BufReader::new(open_file);
-
-        let absolute_path = fs::canonicalize(&self.file)?;
-        let file_path = absolute_path
-            .to_str()
-            .ok_or(std::io::Error::other("None"))?
-            .to_string();
-
-        Ok(ReadResult {
-            path: file_path,
-            file: file_content,
-        })
-    }
-}
 
 pub trait Reader {
     fn read(&self) -> Result<BufReader<File>, crate::error::Error>;
