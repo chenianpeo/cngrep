a3d8f9e1d410fb0e3ecbea5e88cf714479596e44
chenian <chenianpeo@163.com>
2026-06-18 23:06:02 +0800

    refactor(cli): integrated parsing

--- src/cli.rs
+++ src/cli.rs
@@ -42,3 +42,43 @@ pub fn args() -> Result<Cli, crate::error::Error> {
         }),
     }
 }
+
+use std::io::IsTerminal;
+
+use crate::config::{Args, InputSource, Mode};
+
+impl Args {
+    pub fn from_cli(cli: Cli) -> Result<Self, Error> {
+        let input_source: InputSource = determine_input(&cli)?;
+
+        let mode: Mode = determine_mode(&cli)?;
+
+        Ok(Self {
+            query: cli.query,
+            input_source,
+            mode,
+        })
+    }
+}
+
+fn determine_input(cli: &Cli) -> Result<InputSource, Error> {
+    let input_source = if let Some(file) = &cli.file {
+        InputSource::File(file.clone())
+    } else if !std::io::stdin().is_terminal() {
+        InputSource::Stdin
+    } else {
+        InputSource::CurrentDir
+    };
+
+    Ok(input_source)
+}
+
+fn determine_mode(cli: &Cli) -> Result<Mode, Error> {
+    let mode = if cli.count {
+        Mode::CountOnly
+    } else {
+        Mode::Normal
+    };
+
+    Ok(mode)
+}
