6faf8b5538a2f6d06c69e1f8c0d52adb65864ab7
chenian <chenianpeo@163.com>
2026-06-20 00:37:05 +0800

    refactor(cg): use trait replace match

--- src/app.rs
+++ src/app.rs
@@ -1,61 +1,66 @@
 use crate::cli::args;
 use crate::config::Args;
-use crate::config::InputSource;
-use crate::config::Mode;
 use crate::error::Error;
-use crate::matcher::FileMatch;
-use crate::matcher::Match;
-use crate::reader::FileReader;
-use crate::reader::Reader;
+use crate::matcher::type_match;
+// use crate::config::InputSource;
+// use crate::config::Mode;
+// use crate::matcher::FileMatch;
+// use crate::matcher::Match;
+// use crate::reader::FileReader;
+// use crate::reader::Reader;
 
 pub fn run() -> Result<(), Error> {
     let arg: Vec<String> = std::env::args().collect();
     let cli = args(arg)?;
-    println!("{:?}", cli);
 
     let args = Args::from_cli(cli)?;
-    match args.mode {
-        Mode::Normal => match args.input_source {
-            InputSource::File(path) => {
-                let file = FileReader { path };
-                let content = file.read()?;
 
-                let mut file_match = FileMatch {
-                    query: args.query,
-                    file: content,
-                };
+    // match args.mode {
+    //     Mode::Normal => match args.input_source {
+    //         InputSource::File(path) => {
+    //             let file = FileReader { path };
+    //             let content = file.read()?;
 
-                let search_result = file_match.search_normal()?;
-                println!("{:#?}", search_result);
-            }
-            InputSource::Stdin => {
-                println!("stdin");
-            }
-            InputSource::CurrentDir => {
-                println!("current dir")
-            }
-        },
-        Mode::CountOnly => match args.input_source {
-            InputSource::File(path) => {
-                let file = FileReader { path };
-                let content = file.read()?;
+    //             let mut file_match = FileMatch {
+    //                 query: args.query,
+    //                 file: content,
+    //             };
 
-                let mut file_match = FileMatch {
-                    query: args.query,
-                    file: content,
-                };
+    //             let search_result = file_match.search_normal()?;
+    //             println!("{:#?}", search_result);
+    //         }
+    //         InputSource::Stdin => {
+    //             println!("stdin");
+    //         }
+    //         InputSource::CurrentDir => {
+    //             println!("current dir")
+    //         }
+    //     },
+    //     Mode::CountOnly => match args.input_source {
+    //         InputSource::File(path) => {
+    //             let file = FileReader { path };
+    //             let content = file.read()?;
 
-                let search_result = file_match.search_count_only()?;
-                println!("{:#?}", search_result);
-            }
-            InputSource::Stdin => {
-                println!("stdin");
-            }
-            InputSource::CurrentDir => {
-                println!("current dir")
-            }
-        },
-    }
+    //             let mut file_match = FileMatch {
+    //                 query: args.query,
+    //                 file: content,
+    //             };
+
+    //             let search_result = file_match.search_count_only()?;
+    //             println!("{:#?}", search_result);
+    //         }
+    //         InputSource::Stdin => {
+    //             println!("stdin");
+    //         }
+    //         InputSource::CurrentDir => {
+    //             println!("current dir")
+    //         }
+    //     },
+    // }
+
+    let match_result = type_match(args)?;
+    println!("{:?}", match_result);
+    match_result.run()?;
 
     Ok(())
 }
--- src/matcher.rs
+++ src/matcher.rs
@@ -1,49 +1,95 @@
+use std::fmt::Debug;
 use std::fs::File;
-use std::io::BufRead;
 use std::io::BufReader;
+use std::path::PathBuf;
 
-use crate::config::MatchResult;
+use crate::config::InputSource;
+use crate::config::Mode;
 use crate::error::Error;
 
-pub trait Match {
-    fn search_normal(&mut self) -> Result<Vec<MatchResult>, Error>;
-    fn search_count_only(&mut self) -> Result<usize, Error>;
-}
+// pub trait Match {
+//     fn search_normal(&mut self) -> Result<Vec<MatchResult>, Error>;
+//     fn search_count_only(&mut self) -> Result<usize, Error>;
+// }
 
 pub struct FileMatch {
     pub query: String,
     pub file: BufReader<File>,
 }
 
-impl Match for FileMatch {
-    fn search_normal(&mut self) -> Result<Vec<MatchResult>, Error> {
-        let mut search_result: Vec<MatchResult> = Vec::new();
+// impl Match for FileMatch {
+//     fn search_normal(&mut self) -> Result<Vec<MatchResult>, Error> {
+//         let mut search_result: Vec<MatchResult> = Vec::new();
 
-        for (line_no, line) in (&mut self.file).lines().enumerate() {
-            let line = line?;
-            let line_no = line_no + 1;
+//         for (line_no, line) in (&mut self.file).lines().enumerate() {
+//             let line = line?;
+//             let line_no = line_no + 1;
 
-            if line.contains(&self.query) {
-                search_result.push(MatchResult {
-                    line_no,
-                    content: line,
-                })
-            }
-        }
+//             if line.contains(&self.query) {
+//                 search_result.push(MatchResult {
+//                     line_no,
+//                     content: line,
+//                 })
+//             }
+//         }
 
-        Ok(search_result)
-    }
+//         Ok(search_result)
+//     }
 
-    fn search_count_only(&mut self) -> Result<usize, Error> {
-        let mut count: usize = 0;
+//     fn search_count_only(&mut self) -> Result<usize, Error> {
+//         let mut count: usize = 0;
 
-        for line in (&mut self.file).lines() {
-            let line = line.unwrap();
-            if line.contains(&self.query) {
-                count += 1;
-            }
-        }
+//         for line in (&mut self.file).lines() {
+//             let line = line.unwrap();
+//             if line.contains(&self.query) {
+//                 count += 1;
+//             }
+//         }
 
-        Ok(count)
+//         Ok(count)
+//     }
+// }
+
+#[derive(Debug)]
+pub struct NormalFile {
+    pub query: String,
+    pub file: PathBuf,
+}
+
+pub struct CountFile {
+    pub query: String,
+    pub file: PathBuf
+}
+
+// #[derive(Debug)]
+// pub enum Type {
+//     File(NormalFile)
+// }
+use crate::config::Args;
+use crate::reader::Read;
+// use crate::matcher::Type::NormalFile;
+pub fn type_match(args: Args) -> Result<Box<dyn Type>, Error> {
+    match (args.input_source, args.mode) {
+        (InputSource::File(path), Mode::Normal) => Ok(Box::new(NormalFile {query: args.query, file: path})),
+        (InputSource::File(path), Mode::CountOnly) => Ok(Box::new(NormalFile {query: args.query, file: path})),
+        _ => Err(Error::Internal {
+            message: "type match report error".to_string(),
+        }),
     }
 }
+
+pub trait Type: Debug  {
+    fn run(&self) -> Result<(), Error>;
+}
+
+impl Type for NormalFile {
+    fn run(&self) -> Result<(), Error>{
+        self.read();
+
+        Ok(())
+    }
+}
+
+// pub trait Read {
+//     fn read();
+// }
