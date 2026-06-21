a8d392713f718eca9117e1b87b11a6d982c3d6f0
chenian <chenianpeo@163.com>
2026-06-21 21:55:48 +0800

    feat(cli): add simple help info

--- src/cli.rs
+++ src/cli.rs
@@ -2,16 +2,37 @@ use std::path::PathBuf;
 
 use crate::{config::Cli, error::Error};
 
+// input arguments
+// switch to type Struct `Cli` by method
 pub fn args(arg: Vec<String>) -> Result<Cli, crate::error::Error> {
     let mut arg = arg;
     arg.remove(0);
 
+    let help_info = "Not Command\n
+Example Command:
+cngrep [Option] Pattern [Path...]\n
+[Option]\n
+    default                 print search result content
+    --count-only, -c,       print search line number
+
+Pattern\n
+[Path...]\n
+"
+    .to_string();
+
     match arg.len() {
-        1 => Ok(Cli {
-            query: arg[0].clone(),
-            file: None,
-            count: false,
-        }),
+        // process simply handle
+        // existed pattern: arguments number is 1 to 3
+        1 => {
+            if arg[0].clone() == "-h" || arg[0].clone() == "--help" {
+                print!("{}", help_info);
+            }
+            Ok(Cli {
+                query: arg[0].clone(),
+                file: None,
+                count: false,
+            })
+        }
         2 => {
             if arg[0].clone() == "-c" || arg[0].clone() == "--count-only" {
                 Ok(Cli {
@@ -49,14 +70,14 @@ pub fn args(arg: Vec<String>) -> Result<Cli, crate::error::Error> {
             } else {
                 Err(Error::InvalidArgument {
                     r#type: "option".to_string(),
-                    reason: "unknown arguments".to_string(),
+                    reason: help_info,
                 })
             }
         }
 
         _ => Err(Error::InvalidArgument {
             r#type: "arguments".to_string(),
-            reason: "failed command".to_string(),
+            reason: help_info,
         }),
     }
 }
@@ -66,9 +87,13 @@ use std::io::IsTerminal;
 use crate::config::{Args, InputSource, Mode};
 
 impl Args {
+    // associate function
+    // is method if have `self`
     pub fn from_cli(cli: Cli) -> Result<Self, Error> {
+        // decide input source such as Stdin or File
         let input_source: InputSource = determine_input(&cli)?;
 
+        // decide input mode such as Normal or Count Only
         let mode: Mode = determine_mode(&cli)?;
 
         Ok(Self {
