04b1e7a5704efa0303126934233d4c3617c2c7b0
chenian <chenianpeo@163.com>
2026-07-18 18:11:13 +0800

    feat(printer): modify color output code

--- src/printer.rs
+++ src/printer.rs
@@ -1,5 +1,6 @@
+use std::fmt::Display;
+
 use crate::{error::Error, result::MatchResult};
-use owo_colors::OwoColorize;
 
 pub fn render(result: &MatchResult) -> Result<(), Error> {
     match result {
@@ -51,3 +52,43 @@ pub fn render(result: &MatchResult) -> Result<(), Error> {
 
     Ok(())
 }
+
+// pub trait Color {
+//     fn red(&self) -> String;
+//     fn blue(&self) -> String;
+//     fn yellow(&self) -> String;
+// }
+
+// impl<T: Display> Color for T {
+//     fn red(&self) -> String {
+//         format!("\x1b[31m{}\x1b[0m", self)
+//     }
+
+//     fn blue(&self) -> String {
+//         format!("\x1b[34m{}\x1b[0m", self)
+//     }
+
+//     fn yellow(&self) -> String {
+//         format!("\x1b[33m{}\x1b[0m", self)
+//     }
+// }
+
+pub trait Color: Display {
+    fn color(&self, code: u8) -> String {
+        format!("\x1b[{}m{}\x1b[0m", code, self)
+    }
+
+    fn red(&self) -> String {
+        self.color(31)
+    }
+
+    fn yellow(&self) -> String {
+        self.color(33)
+    }
+
+    fn blue(&self) -> String {
+        self.color(34)
+    }
+}
+
+impl<T: Display> Color for T {}
