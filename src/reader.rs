74f37b04cf0eed750f0eac88cba214a7a84d61c0
chenian <chenianpeo@163.com>
2026-07-26 21:25:10 +0800

    feat(reader): split empty input path read

--- src/reader.rs
+++ src/reader.rs
@@ -1,7 +1,5 @@
 use std::{
-    fs::File,
-    io::{BufReader, Stdin},
-    path::PathBuf,
+    env, fs::File, io::{BufReader, IsTerminal, Stdin}, path::PathBuf,
 };
 
 use crate::{cli::Mode, error::Error};
@@ -23,10 +21,9 @@ pub fn read(input_source: &[PathBuf], _mode: &[Mode]) -> Result<ReadResult, Erro
     // need judge stdin or current path when vec is empty
     if input_source.is_empty() {
         let stdin = std::io::stdin();
-        Ok(ReadResult::Stdin(BufReader::new(stdin)))
-    } else if input_source.len() == 1 {
-        if input_source[0].is_dir() {
-            let result = input_source[0]
+
+        if stdin.is_terminal() {
+            let result = env::current_dir()?
                 .read_dir()?
                 .filter_map(|s| {
                     if matches!(
@@ -39,6 +36,37 @@ pub fn read(input_source: &[PathBuf], _mode: &[Mode]) -> Result<ReadResult, Erro
                     }
                 })
                 .map(|entry| {
+                    let path = entry?.path();
+                    let file = File::open(&path)?;
+
+                    Ok(ReadF {
+                        path,
+                        reader: BufReader::new(file),
+                    })
+                })
+                .collect::<Result<Vec<_>, Error>>()?;
+
+            Ok(ReadResult::Dir(result))
+        } else {
+            Ok(ReadResult::Stdin(BufReader::new(stdin)))
+        }
+    } else if input_source.len() == 1 {
+        if input_source[0].is_dir() {
+            let result = input_source[0]
+                .read_dir()?
+                .filter_map(|s| {
+                    // remove nonsupport file
+                    if matches!(
+                        s.as_ref().ok()?.path().extension().and_then(|s| s.to_str()),
+                        Some("pdf" | "epub")
+                    ) {
+                        None
+                    } else {
+                        Some(s)
+                    }
+                })
+                .map(|entry| {
+                    // need recursion ergodic
                     let path = entry?.path();
                     let file = File::open(&path)?;
 
