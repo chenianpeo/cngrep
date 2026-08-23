a9a30190e58ba31b489ca812308050856c45c397
chenian <chenianpeo@163.com>
2026-08-23 21:35:57 +0800

    feat: match content add ignore case

--- src/matcher.rs
+++ src/matcher.rs
@@ -8,7 +8,7 @@ use crate::{
     cli::MatchOptions,
     error::Error,
     printer::{
-        CountMultiFile, CountResult, MatchMultiFile, MatchStdinFile,
+        CountMultiFile, CountResult, IgnoreCaseResult, MatchMultiFile, MatchStdinFile,
         NormalResult::{self},
         SearchResult,
     },
@@ -28,6 +28,10 @@ pub fn search(
                 return stdin.count_search(pattern);
             }
 
+            if mode.contains(&MatchOptions::IgnoreCase) {
+                return stdin.ignore_case_search(pattern);
+            }
+
             stdin.normal_search(pattern)?
         }
 
@@ -36,12 +40,20 @@ pub fn search(
                 return file.count_search(pattern);
             }
 
+            if mode.contains(&MatchOptions::IgnoreCase) {
+                return file.ignore_case_search(pattern);
+            }
+
             file.normal_search(pattern)?
         }
 
         ReadResult::MultiFile(multi_file) => {
             if mode.contains(&MatchOptions::CountOnly) {
                 return multi_file.count_search(pattern);
+            }
+
+            if mode.contains(&MatchOptions::IgnoreCase) {
+                return multi_file.ignore_case_search(pattern);
             }
 
             multi_file.normal_search(pattern)?
@@ -54,6 +66,7 @@ pub fn search(
 pub trait MatchSearch {
     fn normal_search(&self, pattern: &str) -> Result<SearchResult, Error>;
     fn count_search(&self, pattern: &str) -> Result<SearchResult, Error>;
+    fn ignore_case_search(&self, pattern: &str) -> Result<SearchResult, Error>;
 }
 
 fn open_file(path: &PathBuf) -> Result<BufReader<File>, Error> {
@@ -92,6 +105,26 @@ impl MatchSearch for Stdin {
 
         Ok(SearchResult::Count(CountResult::StdinFile(match_number)))
     }
+
+    fn ignore_case_search(&self, pattern: &str) -> Result<SearchResult, Error> {
+        let mut match_result: Vec<MatchStdinFile> = Vec::new();
+        let stdin_lock = self.lock();
+
+        for (line_no, line) in stdin_lock.lines().enumerate() {
+            let line = line?;
+
+            if line.to_lowercase().contains(&pattern.to_lowercase()) {
+                match_result.push(MatchStdinFile {
+                    line_no,
+                    content: line,
+                });
+            }
+        }
+
+        Ok(SearchResult::IgnoreCase(IgnoreCaseResult::StdinFile(
+            match_result,
+        )))
+    }
 }
 
 impl MatchSearch for PathBuf {
@@ -126,6 +159,27 @@ impl MatchSearch for PathBuf {
 
         Ok(SearchResult::Count(CountResult::StdinFile(match_number)))
     }
+
+    fn ignore_case_search(&self, pattern: &str) -> Result<SearchResult, Error> {
+        let mut match_result: Vec<MatchStdinFile> = Vec::new();
+
+        let content = open_file(self)?;
+
+        for (line_no, line) in content.lines().enumerate() {
+            let line = line?;
+
+            if line.to_lowercase().contains(&pattern.to_lowercase()) {
+                match_result.push(MatchStdinFile {
+                    line_no,
+                    content: line,
+                });
+            }
+        }
+
+        Ok(SearchResult::IgnoreCase(IgnoreCaseResult::StdinFile(
+            match_result,
+        )))
+    }
 }
 
 impl MatchSearch for Vec<PathBuf> {
@@ -144,7 +198,7 @@ impl MatchSearch for Vec<PathBuf> {
                     file_match_result.push(MatchStdinFile {
                         line_no,
                         content: line,
-                    });
+                    })
                 }
             }
 
@@ -182,5 +236,37 @@ impl MatchSearch for Vec<PathBuf> {
         }
 
         Ok(SearchResult::Count(CountResult::MultiFile(dir_match)))
+    }
+
+    fn ignore_case_search(&self, pattern: &str) -> Result<SearchResult, Error> {
+        let mut dir_match_result: Vec<MatchMultiFile> = Vec::new();
+
+        for single_file in self {
+            let mut file_match_result: Vec<MatchStdinFile> = Vec::new();
+
+            let content = open_file(single_file)?;
+
+            for (line_no, line) in content.lines().enumerate() {
+                let line = line?;
+
+                if line.to_lowercase().contains(&pattern.to_lowercase()) {
+                    file_match_result.push(MatchStdinFile {
+                        line_no,
+                        content: line,
+                    });
+                }
+            }
+
+            if !file_match_result.is_empty() {
+                dir_match_result.push(MatchMultiFile {
+                    path: single_file.canonicalize()?,
+                    file: file_match_result,
+                });
+            }
+        }
+
+        Ok(SearchResult::IgnoreCase(IgnoreCaseResult::MultiFile(
+            dir_match_result,
+        )))
     }
 }
