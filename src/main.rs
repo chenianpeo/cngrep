7e899d88ebf2caf46c808896f70c29134e527d91
chenian <chenianpeo@163.com>
2026-08-24 19:21:56 +0800

    fix: remove experimental code

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,6 @@ use std::process::ExitCode;
 // todo: exit code should design to be, 0123
 // 0 > success, 1 > not found, 2 > running error
 fn main() -> ExitCode {
-    let _file = cg::os::file::File::open("/home/cn/Code/cngrep/README.md").unwrap();
-
     match run() {
         Ok(_) => ExitCode::from(0),
 
