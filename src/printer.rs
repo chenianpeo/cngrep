0c3841ec5a461eddf26c0c7bc04286e8627d32f1
chenian <chenianpeo@163.com>
2026-08-26 22:06:35 +0800

    chore: remove trashy comments code

--- src/printer.rs
+++ src/printer.rs
@@ -50,181 +50,6 @@ pub trait Color: Display {
 
 impl<T: Display> Color for T {}
 
-// output result
-// pub fn render(pattern: &str, result: &SearchResult, mode: &[OutputOptions]) -> Result<(), Error> {
-//     let mut output_position = OutputPosition::Terminal;
-//     for options in mode {
-//         output_position = match options {
-//             OutputOptions::OutputFile(path) => OutputPosition::File(path.clone()),
-//             _ => OutputPosition::Terminal,
-//         }
-//     }
-
-//     match output_position {
-//         OutputPosition::File(path) => {
-//             let mut output_file = File::create(path)?;
-
-//             match result {
-//                 SearchResult::Normal(normal_result) => match normal_result {
-//                     NormalResult::StdinFile(file_result) => {
-//                         is_matched(file_result)?;
-
-//                         for file in file_result {
-//                             writeln!(output_file, "{}:{}", file.line_no + 1, file.content,)?;
-//                         }
-//                     }
-
-//                     NormalResult::MultiFile(dir_result) => {
-//                         is_matched(dir_result)?;
-
-//                         for (dir_no, dir) in dir_result.iter().enumerate() {
-//                             writeln!(output_file, "{}", dir.path.display())?;
-
-//                             for file in dir.file.iter() {
-//                                 writeln!(output_file, "{}:{}", file.line_no + 1, file.content,)?;
-//                             }
-
-//                             if dir_no != dir_result.len() - 1 {
-//                                 writeln!(output_file)?;
-//                             }
-//                         }
-//                     }
-//                 },
-
-//                 SearchResult::Count(count_result) => match count_result {
-//                     CountResult::StdinFile(stdin_file) => {
-//                         if stdin_file == &0 {
-//                             let not_fount = "Not Found".red();
-//                             return Err(Error::NotFound(not_fount));
-//                         } else {
-//                             writeln!(output_file, "{stdin_file}")?;
-//                         }
-//                     }
-
-//                     CountResult::MultiFile(multi_file) => {
-//                         let mut total_number: usize = 0;
-
-//                         for single_file in multi_file {
-//                             if single_file.number != 0 {
-//                                 total_number += single_file.number;
-//                                 writeln!(
-//                                     output_file,
-//                                     "{}:{}",
-//                                     single_file.path.display(),
-//                                     single_file.number
-//                                 )?;
-//                             }
-//                         }
-
-//                         if total_number == 0 {
-//                             let not_fount = "Not Found".red();
-//                             return Err(Error::NotFound(not_fount));
-//                         }
-
-//                         writeln!(output_file, "Total Match Number: {total_number}")?;
-//                     }
-//                 },
-//             }
-//         }
-
-//         OutputPosition::Terminal => match result {
-//             SearchResult::Normal(normal_result) => match normal_result {
-//                 NormalResult::StdinFile(file_result) => {
-//                     is_matched(file_result)?;
-//                     let pattern_lowercase = pattern.to_lowercase();
-//                     let pattern_len = pattern.len();
-
-//                     for file in file_result {
-//                         let file_content = file.content.clone();
-//                         let mut index_pattern = 0;
-
-//                         if let Some(index) = file_content.to_lowercase().find(&pattern_lowercase) {
-//                             index_pattern = index;
-//                         }
-
-//                         let match_content =
-//                             &file_content[index_pattern..(index_pattern + pattern_len)];
-
-//                         println!(
-//                             "{}:{}",
-//                             (file.line_no + 1).blue(),
-//                             file.content.replace(match_content, &match_content.green())
-//                         );
-//                     }
-//                 }
-
-//                 NormalResult::MultiFile(dir_result) => {
-//                     is_matched(dir_result)?;
-//                     let pattern_lowercase = pattern.to_lowercase();
-//                     let pattern_len = pattern.len();
-
-//                     for (dir_no, dir) in dir_result.iter().enumerate() {
-//                         println!("{}", dir.path.display().yellow());
-
-//                         for file in dir.file.iter() {
-//                             let file_content = file.content.clone();
-//                             let mut index_pattern = 0;
-
-//                             if let Some(index) =
-//                                 file_content.to_lowercase().find(&pattern_lowercase)
-//                             {
-//                                 index_pattern = index;
-//                             }
-//                             let match_content =
-//                                 &file_content[index_pattern..(index_pattern + pattern_len)];
-
-//                             println!(
-//                                 "{}:{}",
-//                                 (file.line_no + 1).blue(),
-//                                 file.content.replace(match_content, &match_content.green())
-//                             );
-//                         }
-
-//                         if dir_no != dir_result.len() - 1 {
-//                             println!();
-//                         }
-//                     }
-//                 }
-//             },
-
-//             SearchResult::Count(count_result) => match count_result {
-//                 CountResult::StdinFile(stdin_file) => {
-//                     if stdin_file == &0 {
-//                         let not_fount = "Not Found".red();
-//                         return Err(Error::NotFound(not_fount));
-//                     }
-//                     println!("{stdin_file}")
-//                 }
-
-//                 CountResult::MultiFile(multi_file) => {
-//                     let mut total_number: usize = 0;
-
-//                     for single_file in multi_file {
-//                         if single_file.number != 0 {
-//                             total_number += single_file.number;
-
-//                             println!(
-//                                 "{}: {}",
-//                                 single_file.path.display().yellow(),
-//                                 single_file.number
-//                             );
-//                         }
-//                     }
-
-//                     if total_number == 0 {
-//                         let not_fount = "Not Found".red();
-//                         return Err(Error::NotFound(not_fount));
-//                     }
-
-//                     println!("Total Match Number: {total_number}");
-//                 }
-//             },
-//         },
-//     }
-
-//     Ok(())
-// }
-
 #[derive(Debug)]
 pub enum SearchResult {
     Normal(Vec<NormalResult>),
@@ -257,19 +82,11 @@ pub struct CountResult {
 }
 
 pub fn output_result(result: &SearchResult, mode: &[OutputOptions]) -> Result<(), Error> {
+    // need redesigned output position options
+    // the for loop is not required
     let mut output_position = OutputPosition::Terminal;
     for options in mode {
         output_position = match options {
-            // OutputOptions::File(file) => {
-
-            // }
-
-            // OutputOptions::Terminal => {
-            //     let stdout = io::stdout();
-            //     let mut writer = stdout.lock();
-
-            //     render(result, &mut writer)?;
-            // }
             OutputOptions::File(file) => OutputPosition::File(file.clone()),
             _ => OutputPosition::Terminal,
         }
@@ -292,10 +109,15 @@ pub fn output_result(result: &SearchResult, mode: &[OutputOptions]) -> Result<()
     Ok(())
 }
 
+// Color output should be control by color mode
+// output result through writeln and default include color
+//
+// render function don't judge output position
 pub fn render<W: Write>(result: &SearchResult, writer: &mut W) -> Result<(), Error> {
     match result {
         SearchResult::Normal(result) => {
             for (no, normal) in result.iter().enumerate() {
+                
                 if let Some(path) = normal.path.clone()
                     && result.iter().len() != 1
                 {
